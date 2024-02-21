350328ed3284f06a19c752e3c66151a9ed0583b2
unknown <venceslavam@gmail.com>
2024-02-21 17:04:13 +0200

    personal profile with name for specific employee added

--- EmployeeDocumentManagementApp/EmployeeDetailsWindow.xaml.cs
+++ EmployeeDocumentManagementApp/EmployeeDetailsWindow.xaml.cs
@@ -18,17 +18,16 @@ namespace EmployeeDocumentManagementApp
         }
         private void OnEmployeeDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            // Get the selected employee from the ListView
             Employee selectedEmployee = (Employee)employeeListView.SelectedItem;
 
             if (selectedEmployee != null)
             {
-                // Create a new instance of the EmployeeProfile window with the selected employee
                 EmployeeProfile profileWindow = new EmployeeProfile(selectedEmployee);
 
-                // Show the EmployeeProfile window
+                profileWindow.Title = "Лично досие - " + selectedEmployee.FullName;
                 profileWindow.Show();
             }
         }
+
     }
 }
